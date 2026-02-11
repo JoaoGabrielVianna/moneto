a55aedfeab7a13d94b667c5da45bc54ef85ea296
joaogabrielvianna <joaogabrielvianna05@gmail.com>
2026-02-11 09:52:05 -0300

    🗑️chore(income-service): remove serviço de income da arquitetura atual

--- services/income-service/internal/migrations/16_08_2025_add_category_id.go
+++
@@ -1,8 +0,0 @@
-package migrations
-
-import "database/sql"
-
-func Up_16_08_2025_add_category_id_to_incomes(db *sql.DB) error {
-	_, err := db.Exec(`ALTER TABLE incomes ADD COLUMN category_id TEXT NOT NULL REFERENCES categories(id);`)
-	return err
-}
