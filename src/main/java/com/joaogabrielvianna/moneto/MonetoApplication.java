f31644d0d24854a18a24551232d1cc8b0f592004
joaogabrielvianna <joaogabrielvianna05@gmail.com>
2026-02-12 14:59:12 -0300

    🏗️chore(core): inicia estrutura base do Moneto com Spring Boot 4 (Java 21

---
+++ src/main/java/com/joaogabrielvianna/moneto/MonetoApplication.java
@@ -0,0 +1,13 @@
+package com.joaogabrielvianna.moneto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonetoApplication.class, args);
+	}
+
+}
